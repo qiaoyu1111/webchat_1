e14bae3af18428a95cf80a7ddc1717247bdac760
qiaoyu <qiaoyu1111358@gmail.com>
2017-07-06 18:31:51 +0800

    friendship : implement

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
-class Message < ApplicationRecord
-end
+ class Message < ActiveRecord::Base
+    belongs_to :conversation
+    belongs_to :user
+    validates_presence_of :body, :conversation_id, :user_id
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friendships
   resources :users, only: [:index]
+
+  resources :conversations do
+    resources :messages
+  end
+ 
   root "friendships#index"
 end
