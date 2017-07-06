69ee6ec932111b9d6cbdaa496d1bae60ce0d9dce
qiaoyu <qiaoyu1111358@gmail.com>
2017-07-06 18:16:22 +0800

    implement friendship

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :friendships
+  resources :users, only: [:index]
+  root "friendships#index"
 end
